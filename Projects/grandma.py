50b17a2aebdff10b0a0cf8bde2944b1b0f88eb26
namlek1 <oscarkelman@seattleacademy.org>
2024-09-18 17:52:09 +0000

    p2.py

---
+++ Projects/grandma.py
@@ -0,0 +1,10 @@
+while True :
+    word=input ("what do you think grandma likes? ")
+
+    if "e" in word:
+        print (f"grandma doesn't like e!")
+    else:
+        print (f"grandma likes everything")
+
+
+    print ("")
---
+++ Projects/p2.py
@@ -0,0 +1,22 @@
+#choice 1 
+
+c1 = input ("your running away from bob whos trying to kill you do you get in a {car} or keep {running}")
+if "car" in c1:
+    print("you get in the car and drive away but then you see bob in a car behind you  ")
+else:
+    print ("bob outruns you and catches you")
+
+ #choice 2 
+ 
+ c2 = input ("do you {stop} at the red light or {run} the light ")
+if "run" in c2: 
+    print ("you speed through the light nearly getting t-boned by a truck after you get passed the intersection you look back and still see bob behind you ")
+else: 
+    print ("you stop your car at the light and bob rear ends causing you to role into the intersection and you get t-boned and die.  ")
+
+    #choice 3 
+    
+    c3 = input ("do you stay on the surface {street} or get on the {highway} ") 
+if "highway" print  ("you get on the high way bob is still behind you ")
+
+
