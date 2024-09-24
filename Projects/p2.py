ec11d8fe052e999c3b4952a38fdeee2641929cfa
namlek1 <oscarkelman@seattleacademy.org>
2024-09-24 17:12:33 +0000

    project 2

--- Projects/p2.py
+++ Projects/p2.py
@@ -1,6 +1,6 @@
 #choice 1 
 
-c1 = input ("your running away from bob whos trying to kill you do you get in a {car} or keep {running}")
+c1 = input ("your running away from bob who's trying to kill you do you get in a [car] or keep [running]")
 if "car" in c1:
     print("you get in the car and drive away but then you see bob in a car behind you  ")
 else:
@@ -8,15 +8,57 @@ else:
 
  #choice 2 
  
- c2 = input ("do you {stop} at the red light or {run} the light ")
-if "run" in c2: 
+c2 = input ("do you [stop] at the red light or go [past] the lIght ")
+if "past " in c2: 
     print ("you speed through the light nearly getting t-boned by a truck after you get passed the intersection you look back and still see bob behind you ")
 else: 
     print ("you stop your car at the light and bob rear ends causing you to role into the intersection and you get t-boned and die.  ")
 
     #choice 3 
     
-    c3 = input ("do you stay on the surface {street} or get on the {highway} ") 
-if "highway" print  ("you get on the high way bob is still behind you ")
+c3 = input ("do you stay on the surface [street] or get on the [highway] ") 
+if "highway" in c3:
+    print  ("you get on the high way bob is still behind you ")
 
+else:
+    print ("you hit a dead end and bob catches you")
+
+    #choice 4 
+
+    c4 = input ("bob is catching up do you [speed] up or [slow] down and try to dodge him. you can also try to pull [over] ")
+
+    if "speed" in c4:
+        print ("you speed up and start getting away but the highway is ending soon")
+
+
+
+    elif "slow"in c4: 
+        print ("he read ends you and you crash and die")
+
+    elif "over" in c4:
+        print ("he rear ends you and kills you")
+
+    c5 = input ("bob is stuck behind a few cars and can't get to you. you can [hid] to the left of a truck in the breakdown lane do or you can keep [driving]")
+
+    if "hide" in c4:
+        print ("he goes by truck and does not see you")
+
+    else:
+        print ("bob catches you and you crash and die")
+    
+
+    c6 = input ("you are now behind bob do you [rear] end him or try to [hit] him off the road you can also not do {anything}")
+
+    if "hit" in c6 or "rear" in c6:
+        print ("you make bob crash and die, you win")
+
+    else:
+        print ("he find you 3 hours later and kills you")
+    
+
+
+
+
+
+    
 
