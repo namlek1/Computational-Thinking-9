92bc4135ef00e35af02f4c2c98c7cb709172ef4f
namlek1 <oscarkelman@seattleacademy.org>
2024-10-02 19:04:13 +0000

    line project

---
+++ Projects/p4.py/grjdf.py
@@ -0,0 +1,24 @@
+import turtle 
+
+t = turtle.Turtle()
+t.goto (100,0)
+t.color ("gold")
+
+for i in range (500) :
+    t.forward (100)
+    t.left (73)
+
+for i in range ( 500 ) : 
+    t.forward( 400 + i)
+    t.left ( 500 )
+
+
+
+
+
+turtle.exitonclick ()
+
+
+
+
+
